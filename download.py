f23720d6248ee3fda81b941930a7884138e8bef3
Lloyd Jackman <lloyd.jackman@thomsonreuters.com>
2017-06-04 21:45:46 +0200

    Corrected JSON config file func

--- download.py
+++ download.py
@@ -3,16 +3,27 @@ import json
 
 mode = input("Use config file? (Y/N) ")
 if mode == "Y":
-    with open('config.json') as config:
+    with open('config.json', 'r') as config:
         data = json.load(config)
-        user = data[ftp][user]
-        password = data[ftp][password]
+        user = data["ftp"]["user"]
+        password = data["ftp"]["password"]
+        feed_series = data["feed"]["series"]
 else:
+    data_dict = {}
     user = input("Please enter your FTP user name: ")
+    data_dict["ftp"] = {}
+    data_dict["ftp"]["user"] = user
     password = input("Please enter your FTP password: ")
+    data_dict["ftp"]["password"] = password
     feed_series = input("Please enter the feed series to be downloaded: ")
+    data_dict["feed"] = {}
+    data_dict["feed"]["series"] = feed_series
+    with open('config.json', 'w') as config:
+        data = json.dump(data_dict, config)
+    
 
 ftp = FTP("lipperftp.thomsonreuters.com", user, password)
 folder_contents = ftp.mlsd()
 for item in folder_contents:
     file_name = item[0]
+    print(file_name)
